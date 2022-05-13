f863de35353013f8aaa9fa54da5ba64ceef7a9a5
elfsvet <stepanmatysik@gmail.com>
2022-05-12 22:26:30 -0400

    redit the employee and wrote tests and added manager with all arguments from employee

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Stepan Matysik Team Profile Generator
 
 You would need to run commands in terminal at the directory of the project:
-$npm install inquirer
-$npm install --save-dev jest
+
+npm install inquirer
+
+npm install --save-dev jest
 
--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,17 +1,32 @@
 const { getMaxListeners } = require('process');
 const Employee = require('../lib/Employee');
 
-test('creates an employee object', () =>{
-    const employee = new Employee('Alex',1,'al.gurino@gmail.com');
+test('creates an employee object', () => {
+    const employee = new Employee('Alex', 1, 'al.gurino@gmail.com');
 
     expect(employee.name).toBe('Alex');
     expect(employee.id).toBe(1);
     expect(employee.email).toBe('al.gurino@gmail.com');
 });
 
-test('gets name value from input', () =>{
-    const input = 'Alex'
-    const employee = new Employee(input);
+test('returns employee\'s name value', () => {
 
-    expect(employee.getName()).toBe(input);
+    const employee = new Employee('Alex');
+
+    expect(employee.getName()).toBe('Alex');
+});
+
+test("returns an employee's id", () => {
+    const employee = new Employee('Alex',1);
+    expect(employee.getId()).toBe(1);
+});
+
+test("returns an employee's email", () => {
+    const employee = new Employee('Alex',1,'al.gurino@gmail.com');
+    expect(employee.getEmail()).toBe('al.gurino@gmail.com');
+});
+
+test("returns an employee's role", () => {
+    const employee = new Employee('Alex',1,'al.gurino@gmail.com');
+    expect(employee.getRole()).toBe('Employee');
 });
