52dade5f8d3bc9d3d7b10e9f13ad3f0d81c91865
elfsvet <stepanmatysik@gmail.com>
2022-05-14 23:19:56 -0400

    fixed the question and copied pages for templates of the website need to reorganise and rewrite site creation also copied the file for write and copy file functions from portfolio creator

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const { join } = require('path');
 const dataArray = [];
 
 
@@ -47,7 +50,8 @@ const managerQuestions = [
 const addTeamOrFinishQuestion = [
     {
         type: 'list',
-        choices: ['Engineer', 'Intern', 'Finish building the team'],
+        name: 'choice',
+        choices: ['Engineer', 'Intern', 'Finish'],
         message: "Would you like to add another teammate?",
     },
 ];
@@ -120,19 +124,84 @@ const internQuestions = [
         }
     },
 ];
-const addQuestion = () => {
-    inquirer.prompt(addTeamOrFinishQuestion)
-    .then(input=>{
-        console.log(input);
+
+const addTeamOrFinish = ()=>{
+   return inquirer.prompt(addTeamOrFinishQuestion)
+   .then(answer=>{
+        switch (answer.choice) {
+            case 'Engineer':
+                // addEngineer();
+                console.log('You chose Engineer');
+                addEngineer();
+                break;
+            case 'Intern':
+                // addIntern();
+                console.log('You chose Intern');
+                addIntern();
+                break;
+            case 'Finish':
+                console.log(dataArray);
+                console.log('You chose Finish');
+                // here needs to be added function of creating a html with all the stuff.
+                break;
+        }
+    })
+}
+
+
+const addManager = () => {
+    //clean the array before start
+    dataArray.length = 0;
+    return inquirer.prompt(managerQuestions)
+    .then(answers => {
+        const manager = new Manager (answers.name, answers.id, answers.email, answers.officeNumber);
+        dataArray.push(manager);
+        console.log(manager);
+        return addTeamOrFinish();
     })
 };
-const promptUser = () => {
-    inquirer.prompt(managerQuestions)
-        .then(input => {
-            console.log(input)
-            dataArray.push(input);
-            addQuestion();
-        })
+
+const addEngineer = () => {
+    return inquirer.prompt(engineerQuestions)
+    .then(answers => {
+        // console.log(answers);
+        const engineer = new Engineer (answers.name,answers.id, answers.email,answers.github);
+        dataArray.push(engineer);
+        console.log(engineer);
+        return addTeamOrFinish();
+    })
+
+}
+
+const addIntern = () => {
+    return inquirer.prompt(internQuestions)
+    .then(answers => {
+        // console.log(answers);
+        // const {name, id, email, github} = answers; a way to deconstruct the object
+        const engineer = new Intern (answers.name,answers.id, answers.email,answers.school);
+        dataArray.push(engineer);
+        console.log(engineer);
+        return addTeamOrFinish();
+    })
+}
+
+addManager()
+
+
+
+// const addQuestion = (managerData) => {
+//    return inquirer.prompt(addTeamOrFinishQuestion)
+//     .then(input=>{
+//         console.log(input);
+//     })
+// };
+// const promptUser = () => {
+//    return inquirer.prompt(managerQuestions)
+//         .then(input => {
+//             console.log(input)
+//             dataArray.push(input);
+//             return input;
+//         })
         // .then(()=> {
         //     inquirer.prompt(addTeamOrFinishQuestion)
         //     .then(input => {
@@ -157,6 +226,6 @@ const promptUser = () => {
     // console.log('Your team is now ready');
     // loop with a question do you want to add a teammate
     // 
-}
+// }
 
-promptUser()
+// promptUser().then(addQuestion())
